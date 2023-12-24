76a92a9341ad64c1355eff0d9374ef95a3561741
VladKozlov <k2v.akosa@mail.ru>
2023-12-24 05:19:48 +0400

    first commit

---
+++ hello.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+<title>Hello World</title>
+</head>
+
+<body>
+	<br/><br/><br/>
+	<?php
+		echo("Hello World!<br />");
+		
+		print("It is Test!!!<br />");
+		
+		echo("Hello Africa!!!<br />");
+		
+		echo("Hello Island!!!<br />");
+	?>
+	<br/><br/><br/>		
+</body>
+</html>
